199bb01e5da988c507e7d7371f9e0cf999a7265b
Zephelion <russellnumo@outlook.com>
2023-03-22 11:41:46 +0100

    fetch created

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,22 @@
-const initialFetchArt = (req, res) => {
-    res.render('index');
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const domain = 'https://www.rijksmuseum.nl/api/nl/collection/';
+const apiKey = process.env.API_KEY;
+var page = 1;
+const limit = 10;
+
+
+const initialFetchArt = async (req, res) => {
+
+    try{
+        const response = await fetch(`${domain}?key=${apiKey}&p=${page}&ps=${limit}`);
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.log(error);
+    }
+    // res.render('index');
 
 };
 
