ce7a7f3a1b337654e443252054d9d0259346017a
Zephelion <russellnumo@outlook.com>
2023-03-23 14:03:34 +0100

    details route

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { initialFetchArt } from '../controllers/homeController.js';
+import { initialFetchArt, getArt } from '../controllers/homeController.js';
 
 
 router.get('/', initialFetchArt);
-// router.get('/', (req, res) => {
-//     res.render('index');
-// });
-
-router.get('/about', (req, res) => {
-    res.send("about");
-    console.log("nu ben je op about")
-});
+router.get('/art/:id', getArt);
 
 
 export default router;
