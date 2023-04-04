4c2f06f06e21bf25fdbdbcd9429358548b0d8889
Zephelion <russellnumo@outlook.com>
2023-04-04 16:09:51 +0200

    offline page rendering

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
 import { initialFetchArt, getArt, loadMoreArt, search } from '../controllers/homeController.js';
+import { offlinePage } from '../controllers/offlineController.js';
 
 
 router.get('/', initialFetchArt);
 router.get('/art/:id', getArt);
 router.get('/loadmore', loadMoreArt);
+router.get('/offline', offlinePage);
 router.post('/search/:searchTerm', search);
 
 
